3a160d536887e5688f00390bd061b6abfd79100a
Siddharth <siddharth2000patel@gmail.com>
2022-11-06 11:25:13 +0530

    project in progress

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const car = new Car(100, 100, 30, 50);
 animate();
 
 function animate(){
+    myCanvas.height = window.innerHeight;
     car.update();
     car.draw(ctx);
+    requestAnimationFrame(animate);
 }
